8bff458fbe44956f85f4c9606713ec34b36ec8c1
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2015-08-05 19:18:44 -0700

    Add another test.

--- argon2_test.go
+++ argon2_test.go
@@ -14,3 +14,14 @@ func TestArgon2(t *testing.T) {
 		t.Errorf("got % x, want % x\n", out, want)
 	}
 }
+
+func TestArgon2_100KB(t *testing.T) {
+	var out [8]byte
+	var msg [16]byte
+	var salt = [8]byte{1,1,1,1,1,1,1,1}
+	argon2(out[:], msg[:], salt[:], nil, nil, 1, 100, 3, t)
+	want := [8]byte{0x8c, 0xba, 0x3f, 0x76, 0xc6, 0x69, 0x66, 0xa6}
+	if want != out {
+		t.Errorf("got % x, want % x\n", out, want)
+	}
+}
