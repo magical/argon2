388bb4eba73791ba0b7736f8b2c5fc182a8bf0d0
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2015-08-07 21:59:47 -0700

    Remove doc.go

--- api.go
+++ api.go
@@ -1,3 +1,14 @@
+// Package argon implements the Argon2 password hashing function as specified
+// in the document
+//
+//     https://password-hashing.net/submissions/specs/Argon-v3.pdf
+//
+// Argon2 comes in two flavors:
+//
+// Argon2i uses data-independent memory access, making it suitable for hashing secret information such as passwords.
+//
+// Argon2d uses data-dependent memory access, making it not suitable for hashing secret information due to potential side-channel attacks.
+//
 package argon
 
 import "errors"
