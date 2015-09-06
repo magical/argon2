fa075cd890dff68c3c5f3545e768327ea49e3bd3
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2015-09-05 23:27:36 -0700

    Speed up round
    The register allocator in Go 1.4 and earlier gives up fairly quickly,
    and our fully-unrolled round function was way over the limit, so
    practically everything was getting spilled.
    
    The solution is to split part of the function out. The round function
    works by running a smaller round function over the rows and columns of
    its input, so let's do that. Another approach would have been a loop
    instead of a function, but that turned out to be slower than this
    approach due to bounds checking.
    
    benchmark                old ns/op     new ns/op     delta
    BenchmarkArgon8KiB       211349        138216        -34.60%
    BenchmarkArgon80KiB      1481035       701139        -52.66%
    BenchmarkArgon800KiB     14058942      6635355       -52.80%
    BenchmarkArgon3N         211348        138166        -34.63%
    BenchmarkArgon10N        533182        272566        -48.88%
    BenchmarkArgon100N       4666105       1999660       -57.14%
    BenchmarkArgon1P         1198741       573925        -52.12%
    BenchmarkArgon2P         1255456       640881        -48.95%
    BenchmarkArgon4P         1362531       762405        -44.04%
    
    benchmark                old MB/s     new MB/s     speedup
    BenchmarkArgon8KiB       38.76        59.27        1.53x
    BenchmarkArgon80KiB      55.31        116.84       2.11x
    BenchmarkArgon800KiB     58.27        123.46       2.12x
    BenchmarkArgon3N         38.76        59.29        1.53x
    BenchmarkArgon10N        15.36        30.06        1.96x
    BenchmarkArgon100N       1.76         4.10         2.33x
    BenchmarkArgon1P         54.67        114.19       2.09x
    BenchmarkArgon2P         52.20        102.26       1.96x
    BenchmarkArgon4P         48.10        85.96        1.79x

--- round.py
+++ round.py
@@ -2,6 +2,25 @@ print("package argon2")
 print()
 print("func block(z, a, b *[128]uint64) {")
 
+for i in range(128):
+    print("\tz[%d] = a[%d] ^ b[%d]" % (i, i, i))
+
+for b in range(0, 128, 16):
+    print("\t_P(" + ", ".join("&z[%d]" % i for i in range(b, b+16)) + ")")
+
+for b in range(0, 16, 2):
+    print("\t_P(" + ", ".join("&z[%d], &z[%d]" % (i, i+1) for i in range(b, 128, 16)) + ")")
+
+for i in range(128):
+    print("\tz[%d] ^= a[%d] ^ b[%d]" % (i, i, i))
+
+print("}")
+print()
+print("func _P("+", ".join("p%d" % i for i in range(16))+" *uint64) {")
+for i in range(16):
+    print("\tvar v%d = *p%d" % (i, i))
+print("\tvar t uint64")
+
 def G(a, b, c, d):
     print("\tt = uint64(uint32(%s)) * uint64(uint32(%s))" % (a, b))
     print("\t%s = %s + %s + t*2" % (a, a, b))
@@ -20,40 +39,16 @@ def G(a, b, c, d):
     print("\t%s = %s ^ %s" % (b, b, c))
     print("\t%s = %s>>63 | %s<<1" % (b, b, b))
 
-def P():
-    G("v0", "v4", "v8", "v12")
-    G("v1", "v5", "v9", "v13")
-    G("v2", "v6", "v10", "v14")
-    G("v3", "v7", "v11", "v15")
-    G("v0", "v5", "v10", "v15")
-    G("v1", "v6", "v11", "v12")
-    G("v2", "v7", "v8", "v13")
-    G("v3", "v4", "v9", "v14")
+G("v0", "v4", "v8", "v12")
+G("v1", "v5", "v9", "v13")
+G("v2", "v6", "v10", "v14")
+G("v3", "v7", "v11", "v15")
+G("v0", "v5", "v10", "v15")
+G("v1", "v6", "v11", "v12")
+G("v2", "v7", "v8", "v13")
+G("v3", "v4", "v9", "v14")
 
 for i in range(16):
-    print("\tvar v%d uint64" % i)
-print("\tvar t uint64")
-print()
-
-for b in range(0, 128, 16):
-    for i, j in zip(range(16), range(b, b+16)):
-        print("\tv%d = a[%d] ^ b[%d]" % (i, j, j))
-    P()
-    for i, j in zip(range(16), range(b, b+16)):
-        print("\tz[%d] = v%d" % (j, i))
-    print()
-
-for b in range(0, 16, 2):
-    for i, j in zip(range(0, 16, 2), range(b, 128, 16)):
-        print("\tv%d = z[%d]" % (i, j))
-        print("\tv%d = z[%d]" % (i+1, j+1))
-    P()
-    for i, j in zip(range(0, 16, 2), range(b, 128, 16)):
-        print("\tz[%d] = v%d" % (j, i))
-        print("\tz[%d] = v%d" % (j+1, i+1))
-    print()
-
-for i in range(128):
-    print("\tz[%d] ^= a[%d] ^ b[%d]" % (i, i, i))
+    print("\t*p%d = v%d" % (i, i))
 
 print("}")
