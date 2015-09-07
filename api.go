82d59eb7dab9a6268371a8c6de2100a2c7357bc6
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2015-09-06 23:57:01 -0700

    Update spec URL and warn about Argon2's in-development status.

--- api.go
+++ api.go
@@ -1,8 +1,11 @@
-// Package argon2 implements the Argon2 password hashing function as specified
-// in the document
+// Package argon2 implements the Argon2 password hashing scheme
+// designed by Alex Biryukov, Daniel Dinu, and Dmitry Khovratovich;
+// and specified in the document
 //
-//     https://github.com/khovratovich/Argon2/raw/d9304a5ecac9fc5b47befd25cbfce6ed026042fe/Argon2.pdf
+//     https://github.com/khovratovich/Argon2/raw/f80a42fdb51e8cd615f9388baad1401b9da66103/Argon2.pdf
 //
+// Warning: This package is currently unstable; Argon2 has not yet been
+// finalized and is still undergoing design tweaks.
 package argon2
 
 import "errors"
